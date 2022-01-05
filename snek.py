3767dd36d034feed19e580ced53bcff59e5df78a
Carl Wiede <carlwie@student.chalmers.se>
2022-01-05 19:32:09 +0100

    created snek and fruit values, added show_score function

--- snek.py
+++ snek.py
@@ -26,5 +26,42 @@ game_window = pygame.display.set_mode((window_x, window_y))
 # fps controller
 fps = pygame.time.Clock()
 
+# define snake default position
+snake_position = [100, 50]
+
+# define initial blocks of the body
+snake_body = [  [100, 50],
+                [90, 50],
+                [80, 50],
+                [70, 50]  
+            ]
+
+# random fruit position
+fruit_position = [random.randrange(1, (window_x//10)) * 10,
+                  random.randrange(1, (window_y//10)) * 10]
+fruit_spawn = True
+
+# default snake direction
+direction = 'RIGHT'
+change_to = direction
+
+# default score
+score = 0
+
+# display score function
+def show_score(choice, color, font, size):
+    
+    # create font object
+    score_font = pygame.font.SysFont(font, size)
+    
+    # display surface object
+    score_surface = score_font.render('Score: ' + str(score), True, color)
+
+    # rectangle for surface object
+    score_rect = score_surface.get_rect()
+    
+    # display the text
+    game_window.blit(score_surface, score_rect)
+
 while True:
     pass
